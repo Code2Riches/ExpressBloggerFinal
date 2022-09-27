9f7b24dc01ddc73d9d6c5d731138596ecbb99d78
Christian <christian@maikens-imac.home>
2022-09-27 14:37:22 -0400

    Get One ID

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,21 +1,45 @@
-const { uuid } = require("uuidv4");
+const {
+  uuid
+} = require("uuidv4");
 var express = require("express");
 var router = express.Router();
 
-const { db } = require("../mongo");
+const {
+  db
+} = require("../mongo");
 
+// Example
 router.get("/get-one-example", async function (req, res, next) {
-  const blogPost = await db()
-    .collection("blogs")
-    .findOne({
-      id: {
-        $exists: true,
-      },
-    });
+  const blogPost = await db().collection("blogs").findOne({
+    id: {
+      $exists: true,
+    }
+  });
   res.json({
     success: true,
     post: blogPost,
   });
+});
+
+router.get('/get-one/:id', async function (req, res, next) {
+  try {
+    const blogId = req.params.id
+
+    const blogPost = await db().collection("blogs").findOne({
+      id: blogId
+    })
+    res.json({
+      success: true,
+      post: blogPost
+    })
+  } catch (err) {
+    //In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+    console.log(err.name)
+    res.json({
+      success: false,
+      error: err.toString()
+    })
+  }
 });
 
 module.exports = router;
